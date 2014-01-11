46af470a32ea555dc5a439cb59406aba472189db
Matthias Noback <matthiasnoback@gmail.com>
2014-01-11 13:54:38 +0100

    Make test using expression argument conditional

--- Tests/Functional/FunctionalTest.php
+++ Tests/Functional/FunctionalTest.php
@@ -6,7 +6,6 @@ use Matthias\SymfonyServiceDefinitionValidator\Compiler\ValidateServiceDefinitio
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class FunctionalTest extends \PHPUnit_Framework_TestCase
@@ -27,18 +26,34 @@ class FunctionalTest extends \PHPUnit_Framework_TestCase
 
     public function testIfTheServiceDefinitionsAreCorrectTheContainerWillBeCompiled()
     {
-        $loader = new XmlFileLoader($this->container, new FileLocator(__DIR__.'/Fixtures'));
+        $loader = new XmlFileLoader($this->container, new FileLocator(__DIR__ . '/Fixtures'));
         $loader->load('correct_service_definitions.xml');
+
+        $this->container->compile();
+    }
+
+    public function testIfAServiceDefinitionWithAnExpressionArgumentIsCorrectTheContainerWillBeCompiled()
+    {
+        if (!class_exists('Symfony\Component\DependencyInjection\ExpressionLanguage')) {
+            $this->markTestSkipped(
+                'Expressions are not supported by this version of the DependencyInjection component'
+            );
+        }
+
+        $loader = new XmlFileLoader($this->container, new FileLocator(__DIR__ . '/Fixtures'));
+        $loader->load('service_definition_with_expression.xml');
 
         $this->container->compile();
     }
 
     public function testIfAServiceDefinitionIsNotCorrectAnExceptionWillBeThrown()
     {
-        $loader = new XmlFileLoader($this->container, new FileLocator(__DIR__.'/Fixtures'));
+        $loader = new XmlFileLoader($this->container, new FileLocator(__DIR__ . '/Fixtures'));
         $loader->load('incorrect_service_definitions.xml');
 
-        $this->setExpectedException('Matthias\SymfonyServiceDefinitionValidator\Exception\InvalidServiceDefinitionsException');
+        $this->setExpectedException(
+            'Matthias\SymfonyServiceDefinitionValidator\Exception\InvalidServiceDefinitionsException'
+        );
         $this->container->compile();
     }
 }
